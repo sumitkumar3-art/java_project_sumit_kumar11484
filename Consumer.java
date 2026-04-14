a7cead9004feb823cdaa081ee8180aab38c99f38
sumitkumar3-art <sumit.kumar3@rungta.org>
2026-04-14 19:56:49 +0530

    Add files via upload

---
+++ Consumer.java
@@ -0,0 +1,13 @@
+package my_project;
+
+public class Consumer {
+    int id;
+    String name;
+    int units;
+
+    Consumer(int id, String name, int units) {
+        this.id = id;
+        this.name = name;
+        this.units = units;
+    }
+}
